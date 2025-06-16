53ae928e3de0b22b39a8eddbfcf547a892a5a72f
abhish-1 <abhishekkant135@gmail.com>
2025-06-16 23:40:15 +0530

    User authentication done using Clerk.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -15,9 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${inter.className} `}>
         <Header />
+        
         <main className="min-h-screen">{children}</main>
 
         <footer className="bg-blue-50 py-12">
@@ -27,5 +32,6 @@ export default function RootLayout({ children }) {
           </footer>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex justify-end items-center p-4 gap-4 h-16">
+        <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+        </div>
   )
 }
 
